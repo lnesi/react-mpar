21b63701ee6863e01d634d670d00b673079ba46e
Luis Nesi <luis.nesi@tests-mbp.sbank.uk.ibm.com>
2019-05-31 15:59:46 +0100

    docs wip continues

--- README.md
+++ README.md
@@ -132,14 +132,14 @@ const dictionary = {
     AsyncTestComponent: {
         classLoader:()=>import("./AsyncTestComponent"),
         name: "Async Test React Component",
-        description: "This is a standalone react component can be either a single funcitonal component or a complete SPA",
+        description: "This is a standalone react component can be either a single functional component or a complete SPA",
         reduxEnabled: false,
         createState: false,
     },
     TestComponent: {
         class:TestComponent,
         name: "Test React Component",
-        description: "This is a standalone react component can be either a single funcitonal component or a complete SPA",
+        description: "This is a standalone react component can be either a single functional component or a complete SPA",
         reduxEnabled: false,
         createState: false,
     },
--- src/dictionaries/development.js
+++ src/dictionaries/development.js
@@ -4,14 +4,14 @@ export default  {
     AsyncTestComponent: {
         classLoader:()=>import("../components/AsyncTestComponent"),
         name: "Async Test React Component",
-        description: "This is a standalone react component can be either a single funcitonal component or a complete SPA",
+        description: "This is a standalone react component can be either a single functional component or a complete SPA",
         reduxEnabled: false,
         createState: false,
     },
     SyncTestComponent: {
         class:SyncTestComponent,
         name: "Sync Test React Component",
-        description: "This is a standalone react component can be either a single funcitonal component or a complete SPA",
+        description: "This is a standalone react component can be either a single functional component or a complete SPA",
         reduxEnabled: false,
         createState: false,
     },
